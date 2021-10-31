6af1348d5c3388e096afba25a738428718a45e71
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-10-31 23:03:28 +0100

    Welcome's page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,13 +27,13 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <x-slot name="logo">
-                        <a href="/">
-                            <x-application-logo class="w-40 h-40 fill-current text-blue-500" />
-                        </a>
-                    </x-slot>
                     @if (Route::has('login'))
                         <div class="p-6">
+                            <x-slot name="logo">
+                                <a href="/">
+                                    <x-application-logo class="w-40 h-40 fill-current text-blue-500" />
+                                </a>
+                            </x-slot>
                             @auth
                                 <p class="rounded-lg bg-blue-600 text-white px-8 py-4 text-3xl"><a href="{{ url('/dashboard') }}">Dashboard</a></p>
                             @else
