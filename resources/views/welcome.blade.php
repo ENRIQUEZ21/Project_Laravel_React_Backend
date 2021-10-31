ebf71dfa70297e0b2b00c6f15a92f5643e10f271
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-10-31 23:31:51 +0100

    Corrections of errors linked to admin mode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,10 +27,6 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
-                <p><a href="/"><i class="fa fa-user"></i>
-                        <i class="fa fa-cutlery rounded-lg bg-blue-600 text-white px-8 py-4 text-3xl"></i>
-                    </a></p>
-
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     @if (Route::has('login'))
                         <div class="p-6">
