6786eb8fa323e7cd480fc89e06b8448f8b369faa
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-10-31 22:39:21 +0100

    Welcome's page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,12 +29,12 @@
                     @if (Route::has('login'))
                         <div class="p-6">
                             @auth
-                                <a href="{{ url('/dashboard') }}" class="text-sm text-red-600 underline">Dashboard</a>
+                                <a href="{{ url('/dashboard') }}" class="text-sm text-green underline">Dashboard</a>
                             @else
-                                <a href="{{ route('login') }}" class="text-sm text-red-700 underline">Log in</a>
+                                <a href="{{ route('login') }}" class="text-sm text-red underline">Log in</a>
 
                                 @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-green-700 underline">Register</a>
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-red underline">Register</a>
                                 @endif
                             @endauth
                         </div>
