b5ba859abdbd396ff16a4bbdf127000a606b95c4
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-10-31 22:27:34 +0100

    Welcome's page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,14 +27,14 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     @if (Route::has('login'))
-                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        <div class="hidden fixed">
                             @auth
-                                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                                <a href="{{ url('/dashboard') }}" class="text-sm text-red-700 underline">Dashboard</a>
                             @else
-                                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+                                <a href="{{ route('login') }}" class="text-sm text-red-700 underline">Log in</a>
 
                                 @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-green-700 underline">Register</a>
                                 @endif
                             @endauth
                         </div>
